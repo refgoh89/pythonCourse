8bdff75ee7f9e8b94ac42e36fcfba2f7d7ac1be5
Fernando Gomez Hernandez <refgoh89@gmail.com>
2018-01-04 16:51:03 -0600

    Hola Mundo en Python

---
+++ code1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__execution__ ="python code1.py"
+#---------------------------
+#Library 
+#import <lib>
+import random
+
+#---------------------------
+def myfirstHellow():
+	print ("Hola Mundo")
+
+
+#---------------------------
+if __name__ == "__main__":
+	myfirstHellow()
+
+#---------------------------
